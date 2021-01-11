5f1e54a22f89fe8ff0cabb5d7d9cbb4132e8d46b
Stormtorch <pianister123@gmail.com>
2021-01-10 19:22:53 -0500

    Remove local cache

--- cogs/npc.py
+++ cogs/npc.py
@@ -10,31 +10,11 @@ class NPC(commands.Cog):
         self.bot = bot
         self.db = self.bot.db
         self.webhook = None
-        self.npcs = {}
         self.session = None
         self.webhooks = {
             725164675679125515: 796591431980285984,
             725093929481142292: 750402737530732615
         }
-        self.bot.loop.create_task(self.get_npcs())
-
-    def cog_unload(self):
-        self.bot.loop.create_task(self.close())
-
-    async def close(self):
-        await self.session.close()
-
-    async def get_npcs(self):
-        self.webhooks = {k: await self.bot.fetch_webhook(v) for k, v in self.webhooks.items()}
-        self.session = aiohttp.ClientSession()
-        query = 'SELECT username, avatar_url, user_id, alias FROM npcs'
-        res = await self.db.fetch(query)
-        for res in res:
-            self.npcs[res['alias'].lower()] = {
-                'username': res['username'],
-                'avatar_url': res['avatar_url'],
-                'user_id': res['user_id'],
-            }
 
     @commands.command()
     async def replay(self, ctx, channel: discord.TextChannel, msg_1: int, msg_2: int):
@@ -76,15 +56,15 @@ class NPC(commands.Cog):
         You can add/remove NPCs with the listed subcommands.
 
         **Usage:** `$npc <alias> <message>`"""
-        if alias.lower() not in self.npcs:
+        res = await self.db.fetchrow('SELECT avatar_url, username FROM npcs WHERE alias = $1', alias.lower())
+        if not res:
             return await ctx.send(f'There isn\'t an NPC with alias `{alias}`.')
-        npc = self.npcs[alias.lower()]
         if ctx.channel.id not in self.webhooks:
             return await ctx.send('NPCs can only be used in bot-cmds and general.')
         await self.webhooks[ctx.channel.id].send(
             message,
-            avatar_url=npc['avatar_url'],
-            username=npc['username'],
+            avatar_url=res['avatar_url'],
+            username=res['username'],
             allowed_mentions=self.bot.allowed_mentions
         )
         await ctx.message.delete()
@@ -94,7 +74,7 @@ class NPC(commands.Cog):
         """Adds an NPC.
 
            **Usage:** `$npc add <alias> <username> <pfp url>`"""
-        if alias.lower() in self.npcs:
+        if await self.db.fetchrow('SELECT id FROM npcs WHERE alias = $1', alias.lower()):
             return await ctx.send(f'Alias `{alias}` already in use.')
         if len(alias) > 16:
             return await ctx.send('Alias must be 16 characters or lower.')
@@ -110,11 +90,6 @@ class NPC(commands.Cog):
             avatar = pfp
         else:
             avatar = ''
-        self.npcs[alias.lower()] = {
-            'username': username,
-            'avatar_url': avatar,
-            'user_id': ctx.author.id,
-        }
         query = 'INSERT INTO npcs (user_id, username, avatar_url, alias) VALUES ($1, $2, $3, $4)'
         await self.db.execute(query, ctx.author.id, username, avatar, alias.lower())
         await ctx.send('Your NPC has been created!')
@@ -125,15 +100,14 @@ class NPC(commands.Cog):
            You must own the NPC or have Admin to delete it.
 
            **Usage:** `$npc delete <alias>`"""
-        if alias.lower() in self.npcs:
+        res = await self.db.fetchrow('SELECT user_id, username FROM npcs WHERE alias = $1', alias.lower())
+        if not res:
             return await ctx.send(f'An NPC with alias `{alias}` doesn\'t exist.')
-        if self.npcs[alias.lower()]['user_id'] != ctx.author.id and \
-                not ctx.author.guild_permissions.administrator:
+        if res['user_id'] != ctx.author.id and not ctx.author.guild_permissions.administrator:
             return await ctx.send('You have to either own the NPC or be Admin to delete it.')
-        npc = self.npcs.pop(alias.lower())
         query = 'DELETE FROM npcs WHERE alias = $1'
         await self.db.execute(query, alias.lower())
-        await ctx.send(f'Successfully deleted NPC `{npc["username"]}`.')
+        await ctx.send(f'Successfully deleted NPC `{res["username"]}`.')
 
 
 def setup(bot):
